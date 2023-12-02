19d8b85ff9f6484b8007df5c3bcd5674edc06ab4
NikKms <mykola.kamyshnikov@gmail.com>
2023-12-02 23:23:48 +0200

    change switcher

--- app/ui/header/localeSwitcher/LocaleSwitcher.jsx
+++ app/ui/header/localeSwitcher/LocaleSwitcher.jsx
@@ -2,11 +2,12 @@
 
 import { usePathname } from 'next/navigation';
 import { i18n } from '@/i18n.config';
-import { List, ListItem, VisuallyHiddenInput } from '@chakra-ui/react';
+import { Box, List, ListItem, VisuallyHiddenInput } from '@chakra-ui/react';
 import { useFormState } from 'react-dom';
 import { createCookie } from '@/app/lib/actions';
 import SubmitButton from '../../submitButton/SubmitButton';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 const LocaleSwitcher = () => {
   const pathName = usePathname();
@@ -20,16 +21,41 @@ const LocaleSwitcher = () => {
   const [state, dispatch] = useFormState(createCookie, undefined);
 
   return (
-    <List>
+    <List display={'flex'}>
       <ListItem>
         <form action={dispatch}>
           <VisuallyHiddenInput name={'lang'} defaultValue={i18n.locales[0]} />
           <VisuallyHiddenInput
             name={'path'}
             value={path}
-            onChange={(e) => setPath(e.target.value)}
+            onChange={e => setPath(e.target.value)}
           />
-          <SubmitButton bgColor={'transparent'}>{i18n.locales[0]}</SubmitButton>
+          <SubmitButton
+            bgColor={'transparent'}
+            px={'2px'}
+            hover={'transparent'}
+          >
+            <Box
+              display={'block'}
+              pos={'relative'}
+              w={'40px'}
+              h={'30px'}
+              overflow={'hidden'}
+              borderRadius={'10px'}
+            >
+              <Image
+                src={'/gb.svg'}
+                alt={'product image'}
+                fill
+                style={{
+                  display: 'block',
+                  height: '100%',
+                  maxWidth: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            </Box>
+          </SubmitButton>
         </form>
       </ListItem>
       <ListItem>
@@ -38,10 +64,31 @@ const LocaleSwitcher = () => {
 
           <VisuallyHiddenInput
             name={'path'}
-            onChange={(e) => setPath(e.target.value)}
+            onChange={e => setPath(e.target.value)}
             value={path}
           />
-          <SubmitButton bgColor={'transparent'}>{i18n.locales[1]}</SubmitButton>
+          <SubmitButton bgColor={'transparent'} hover={'transparent'}>
+            <Box
+              display={'block'}
+              pos={'relative'}
+              w={'40px'}
+              h={'30px'}
+              overflow={'hidden'}
+              borderRadius={'10px'}
+            >
+              <Image
+                src={'/he.svg'}
+                alt={'product image'}
+                fill
+                style={{
+                  display: 'block',
+                  height: '100%',
+                  maxWidth: '100%',
+                  objectFit: 'cover',
+                }}
+              />
+            </Box>
+          </SubmitButton>
         </form>
       </ListItem>
     </List>
