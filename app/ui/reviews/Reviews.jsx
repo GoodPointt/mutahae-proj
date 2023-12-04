a648beaac84c5dd57008e59bb94610d26d957d64
vadimfront <vadimzila94@gmail.com>
2023-12-04 13:57:01 +0200

    remove console.log

--- app/ui/reviews/Reviews.jsx
+++ app/ui/reviews/Reviews.jsx
@@ -5,7 +5,6 @@ import { fetchReviews } from '../../lib/api/instance';
 
 const Reviews = async ({ lang, dictionary }) => {
   const { data: reviews } = await fetchReviews(lang);
-  console.log(reviews);
   return (
     <SectionWrapper heading={dictionary.header.navItems[3].title}>
       <ReviewsSlider reviews={reviews} lang={lang} />
