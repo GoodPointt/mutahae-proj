5cdd80ed6f711f4d48fb4763d7c129bdceca29de
NikKms <mykola.kamyshnikov@gmail.com>
2023-12-07 14:19:57 +0200

    optimization lists in topBar and footercontacts

--- app/ui/footer/contactsFooter/ContactsFooter.jsx
+++ app/ui/footer/contactsFooter/ContactsFooter.jsx
@@ -1,53 +1,68 @@
 import { Box, Flex, Link, Text } from '@chakra-ui/react';
 import { FaLocationDot, FaPhone, FaEnvelope } from 'react-icons/fa6';
 
-const ContactsFooter = ({
-  contacts: { address, phone, email, addressUrl },
-  lang,
-  dictionary,
-}) => {
-  const iconLink = (icon, text, href) => (
-    <Link
-      href={href || '/'}
-      target={!href.includes('tel') ? '_blank' : null}
-      rel="noopener noreferrer nofollow"
-      display="flex"
-      alignItems="center"
-      color="#a28445"
-      transition={'all 0.3s'}
-      _hover={{ color: '#81672e' }}
-    >
-      {icon}
-      <Box as="div" mx={2}>
-        <Text
-          fontSize={{ base: '14px', lg: '16px', xl: '18px' }}
-          dir={text === address && lang === 'he' ? 'rtl' : 'ltr'}
-        >
-          {text || ''}
-        </Text>
-      </Box>
-    </Link>
+const ContactsFooter = ({ contacts, lang, dictionary }) => {
+  const iconData = [
+    {
+      icon: <FaLocationDot size="24" />,
+      text: contacts.address,
+      href: contacts.addressUrl,
+    },
+    {
+      icon: <FaPhone size="24" />,
+      text: `+${contacts.phone}`,
+      href: `tel:+${contacts.phone}`,
+    },
+    {
+      icon: <FaEnvelope size="24" />,
+      text: contacts.email,
+      href: `mailto:${contacts.email}`,
+    },
+  ];
+
+  const renderIconLink = ({ icon, text, href }) => (
+    <Box as="li" key={href}>
+      <Link
+        key={text}
+        href={href || '/'}
+        target={!href.includes('tel') ? '_blank' : null}
+        rel="noopener noreferrer nofollow"
+        display="flex"
+        alignItems="center"
+        color="#a28445"
+        transition={'all 0.3s'}
+        _hover={{ color: '#81672e' }}
+      >
+        {icon}
+        <Box as="div" mx={2}>
+          <Text
+            fontSize={{ base: '14px', lg: '16px', xl: '18px' }}
+            dir={text === contacts.address && lang === 'he' ? 'rtl' : 'ltr'}
+          >
+            {text || ''}
+          </Text>
+        </Box>
+      </Link>
+    </Box>
   );
 
   return (
     <Flex
       justifyContent="space-between"
-      alignItems={'flex-start'}
-      flexDirection={'column'}
-      gap={'20px'}
+      alignItems="flex-start"
+      flexDirection="column"
+      gap="20px"
     >
       <Text>{dictionary.footer.contacts}</Text>
       <Box
-        as="div"
+        as="ul"
         display="flex"
-        flexDirection={'column'}
-        alignItems={'flex-start'}
+        flexDirection="column"
+        alignItems="flex-start"
         py={{ base: '12px' }}
         gap={{ base: '18px', lg: '14px', xl: '16px' }}
       >
-        {iconLink(<FaLocationDot size="24" />, address, addressUrl)}
-        {iconLink(<FaPhone size="24" />, `+${phone}`, `tel:+${phone}`)}
-        {iconLink(<FaEnvelope size="24" />, email, `mailto:${email}`)}
+        {iconData.map(renderIconLink)}
       </Box>
     </Flex>
   );
