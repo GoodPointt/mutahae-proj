1ee5fee47d31cfae2097e3302442f501574ea6c9
NikKms <mykola.kamyshnikov@gmail.com>
2023-12-07 15:50:40 +0200

    fix links in social links, change media in team

--- app/ui/socialLinks/SocialLinks.jsx
+++ app/ui/socialLinks/SocialLinks.jsx
@@ -12,44 +12,57 @@ const SocialLinks = async ({ lang, pos = 'fixed' }) => {
 
   const { insta, fb, watsapp, tg } = contacts;
 
+  const socialLinks = [
+    { link: watsapp, icon: <BsWhatsapp size={28} />, label: 'whatsapp' },
+    { link: tg, icon: <BsTelegram size={28} />, label: 'telegram' },
+    { link: fb, icon: <BsFacebook size={28} />, label: 'facebook' },
+    { link: insta, icon: <BsInstagram size={28} />, label: 'instagram' },
+  ];
+
   const createLink = (href, icon, aria) => (
-    <Link
-      href={href}
-      aria-label={aria}
-      target="_blank"
-      rel="noopener noreferrer nofollow"
-      display={'flex'}
-      justifyContent={'center'}
-      alignItems={'center'}
-      boxSize={'40px'}
-      borderRadius={'4px'}
-      bgColor={'#a9884173'}
-      color={'#ccc'}
-      _hover={{
-        transform: 'translateX(-5px)',
-        cursor: 'pointer',
-        bgColor: '#a98841',
-      }}
-      transition={'all 0.3s'}
-    >
-      {icon}
-    </Link>
+    <Box as="li" key={aria}>
+      <Link
+        href={href}
+        aria-label={aria}
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        boxSize={'40px'}
+        borderRadius={'4px'}
+        bgColor={'#a9884173'}
+        color={'#ccc'}
+        _hover={{
+          transform: lang === 'en' ? 'translateX(-5px)' : 'translateX(5px)',
+          cursor: 'pointer',
+          bgColor: '#a98841',
+        }}
+        transition={'all 0.3s'}
+      >
+        {icon}
+      </Link>
+    </Box>
   );
+
+  const renderedLinks = socialLinks.map(({ link, icon, label }) =>
+    createLink(link, icon, label)
+  );
+
+  const position = lang === 'en' ? 'right' : 'left';
 
   return (
     <Box
+      as="ul"
       position={pos}
       bottom={'20%'}
-      right={'0'}
+      style={{ [position]: 0 }}
       display={'flex'}
       flexDirection={'column'}
       gap={'3px'}
       zIndex="98"
     >
-      {createLink(watsapp, <BsWhatsapp size={28} />, 'whatsapp')}
-      {createLink(tg, <BsTelegram size={28} />, 'telegram')}
-      {createLink(fb, <BsFacebook size={28} />, 'facebook')}
-      {createLink(insta, <BsInstagram size={28} />, 'instagram')}
+      {renderedLinks}
     </Box>
   );
 };
