1c66613e20ed97f25dbf8e3c4150a584341229de
NikKms <mykola.kamyshnikov@gmail.com>
2023-12-15 17:50:08 +0200

    add favicon in sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -19,6 +19,12 @@ export default async function sitemap() {
 
   return [
     {
+      url: process.env.NEXT_PUBLIC_URL + '/favicon.ico',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+    {
       url: process.env.NEXT_PUBLIC_URL + '/en',
       lastModified: new Date(),
       changeFrequency: 'yearly',
