6afdaeb956143557df1e849b2619b1cc7a116840
LosOleksandr <losalexander2003@gmail.com>
2024-01-27 18:38:22 +0200

    feat: restyle single product page

---
+++ app/ui/singleProduct/singleProductSlider/SingleProductSlider.jsx
@@ -0,0 +1,80 @@
+'use client';
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+
+import { Box, Button, Flex, List, ListItem } from '@chakra-ui/react';
+
+import './singleProductSlider.css';
+
+const SingleProductSlider = ({ imgs }) => {
+	const startImg = imgs.length !== 0 ? imgs[0].attributes.url : null;
+	const [currentImg, setCurrentImg] = useState(startImg);
+
+	return (
+		<Flex
+			gap={'10px'}
+			display={{ base: 'block', lg: 'flex' }}
+			flexDir={'row-reverse'}
+			maxW={'100%'}
+		>
+			<Box
+				w={'450px'}
+				h={'450px'}
+				pos={'relative'}
+				mb={{ base: '10px', lg: 0 }}
+			>
+				<Image
+					src={currentImg || '/img/product.png'}
+					alt="img"
+					fill
+					style={{ objectFit: 'cover' }}
+				/>
+			</Box>
+
+			{Array.isArray(imgs) && imgs.length !== 0 && (
+				<List
+					display={'flex'}
+					maxHeight={'450px'}
+					maxW={'100%'}
+					flexDir={{ base: 'row', lg: 'column' }}
+					gap={'10px'}
+					overflow={'auto'}
+					className="list"
+				>
+					{imgs.map(({ id, attributes: img }) => (
+						<ListItem key={id}>
+							<Button
+								w={'80px'}
+								h={'80px'}
+								borderRadius={0}
+								border={
+									currentImg === img.url
+										? '2px rgb(162, 132, 69) solid'
+										: 'none'
+								}
+								_hover={{
+									bg: currentImg === img.url ? 'rgba(0,0,0, 0.5)' : 'inherit',
+								}}
+								bg={currentImg === img.url ? 'rgba(0,0,0, 0.5)' : ''}
+								onClick={() => setCurrentImg(img.url)}
+							>
+								<Image
+									src={img.url}
+									alt={img.name}
+									fill
+									style={{
+										objectFit: 'cover',
+										zIndex: '-1',
+									}}
+								/>
+							</Button>
+						</ListItem>
+					))}
+				</List>
+			)}
+		</Flex>
+	);
+};
+
+export default SingleProductSlider;
