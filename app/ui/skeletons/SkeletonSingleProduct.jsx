61675350b3e8059e82fd5dcbe09f962438b76ce9
LosOleksandr <losalexander2003@gmail.com>
2024-03-06 18:23:42 +0200

    fix: skeleton styles

--- app/ui/skeletons/SkeletonSingleProduct.jsx
+++ app/ui/skeletons/SkeletonSingleProduct.jsx
@@ -3,101 +3,61 @@ import { Box, Flex, Grid, Skeleton, SkeletonText } from '@chakra-ui/react';
 const SkeletonSingleProduct = () => {
 	return (
 		<>
-			<Box maxW={'300px'}>
-				<SkeletonText
-					mb={'30px'}
-					noOfLines={1}
-					skeletonHeight="2"
-					startColor="#ad9a74"
-					endColor="gray.400"
-				/>
-			</Box>
-			<Grid
-				templateColumns={{ base: '1fr', lg: 'repeat(2, 1fr)' }}
-				gap={'50px'}
-				maxW={'100%'}
-			>
-				<Skeleton
-					display={'block'}
-					w={'550px'}
-					h={'380px'}
-					overflow={'hidden'}
-					borderRadius={'10px'}
-					startColor="#ad9a74"
-					endColor="gray.400"
-				/>
-
-				<Flex flexDir={'column'} gap={'30px'}>
-					<Box maxW={'360px'}>
-						<SkeletonText
-							noOfLines={1}
-							spacing="1"
-							skeletonHeight="9"
-							startColor="#ad9a74"
-							endColor="gray.400"
-						/>
-					</Box>
-					<Box maxW={'200px'}>
-						<SkeletonText
-							mb={'30px'}
-							noOfLines={1}
-							spacing="1"
-							skeletonHeight="5"
-							startColor="#ad9a74"
-							endColor="gray.400"
-						/>
-						<SkeletonText
-							noOfLines={6}
-							spacing="4"
-							skeletonHeight="2"
-							startColor="#ad9a74"
-							endColor="gray.400"
-						/>
-					</Box>
-					<Grid
-						templateColumns={{ base: '1fr', sm: '1.50fr 2fr' }}
-						gap={'10px'}
-						mt={{ base: '30px', lg: 'auto' }}
-						alignItems={'center'}
-					>
-						<SkeletonText
-							noOfLines={1}
-							spacing="1"
-							skeletonHeight="9"
-							startColor="#ad9a74"
-							endColor="gray.400"
-							w={'100%'}
-						/>
-						<SkeletonText
-							noOfLines={1}
-							spacing="1"
-							skeletonHeight="9"
-							startColor="#ad9a74"
-							endColor="gray.400"
-							w={'100%'}
-						/>
-					</Grid>
-				</Flex>
-			</Grid>
-			<Box mt={'50px'}>
-				<Box maxW={'150px'}>
-					<SkeletonText
-						mt={4}
-						noOfLines={1}
-						spacing="2"
-						skeletonHeight="9"
+			<Box>
+				<Box maxW={'300px'}>
+					<SkeletonText mb={'30px'} noOfLines={1} skeletonHeight="2" />
+				</Box>
+				<Grid
+					templateColumns={{ base: '1fr', lg: 'repeat(2, 1fr)' }}
+					gap={'50px'}
+					maxW={'100%'}
+				>
+					<Skeleton
+						display={'block'}
+						w={'550px'}
+						h={'450px'}
+						overflow={'hidden'}
+						borderRadius={'10px'}
 						startColor="#ad9a74"
 						endColor="gray.400"
 					/>
-				</Box>
-				<SkeletonText
-					mt={4}
-					noOfLines={6}
-					spacing="2"
-					skeletonHeight="2"
-					startColor="#ad9a74"
-					endColor="gray.400"
-				/>
+
+					<Flex flexDir={'column'} gap={'30px'}>
+						<Box maxW={'360px'}>
+							<SkeletonText noOfLines={1} spacing="1" skeletonHeight="9" />
+						</Box>
+						<Box maxW={'200px'}>
+							<SkeletonText
+								mb={'30px'}
+								noOfLines={1}
+								spacing="1"
+								skeletonHeight="6"
+							/>
+							<Box maxW={'60px'}>
+								<SkeletonText noOfLines={6} spacing="4" skeletonHeight="2" />
+							</Box>
+						</Box>
+						<Grid
+							templateColumns={{ base: '1fr', sm: '1.45fr 2fr' }}
+							gap={'10px'}
+							mt={{ base: '30px', lg: 'auto' }}
+							alignItems={'center'}
+						>
+							<SkeletonText
+								noOfLines={1}
+								spacing="1"
+								skeletonHeight="9"
+								w={'100%'}
+							/>
+							<SkeletonText
+								noOfLines={1}
+								spacing="1"
+								skeletonHeight="9"
+								w={'100%'}
+							/>
+						</Grid>
+					</Flex>
+				</Grid>
 			</Box>
 		</>
 	);
